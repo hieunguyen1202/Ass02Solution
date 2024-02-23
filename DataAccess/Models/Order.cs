9df31f3f7129fc7118076c28d42b322658428a37
nguyenhieu1202 <hieunguyenhd2003@gmail.com>
2024-02-23 09:21:41 +0700

    Add project files.

---
+++ DataAccess/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+
+        public int MemberId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime? RequiredDate { get; set; }
+
+        public DateTime? ShippedDate { get; set; }
+
+        public decimal? Freight { get; set; }
+
+        public virtual Member Member { get; set; }
+
+        public virtual OrderDetail OrderDetail { get; set; }
+    }
+}
